9ead6495ae8dba354d30544c59d5403597849663
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 23:07:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(deli)
+end
+
+def take_number(deli, name)
+  deli.push(name)
+  deli.size
+end
+
+def now_serving(deli)
+  if deli.size ? 0
+    puts deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
